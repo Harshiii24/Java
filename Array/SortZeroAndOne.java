f79f3e3f63dd5b47fc2fac9fbc93d07345262762
Harshal Patil <harshalpatil24082003@gmail.com>
2025-08-11 14:36:26 +0530

    Fileas Added

---
+++ Array/SortZeroAndOne.java
@@ -0,0 +1,26 @@
+package strings;
+
+import java.util.Arrays;
+
+public class SortZeroAndOne {
+	public static void main(String[] args)
+	{
+		int[] ar = {1,0,1,1,1,0,1,1,0,1,0,1,0,0,1,1,0,1};
+		
+		for(int i = 0 ; i<ar.length-1; i++)
+		{
+			for(int j = 0; j<ar.length-1-i;j++)
+			{
+				if(ar[j]>ar[j+1])
+				{
+					int temp = ar[j];
+					ar[j] = ar[j+1];
+					ar[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(ar));
+	}
+}
+
+//O/p : [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
